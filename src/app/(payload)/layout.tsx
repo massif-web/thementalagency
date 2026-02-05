ca6bb143e719292f507fb4fbe2a0f96dccff55c4
ynamite <ynamite@bluewin.ch>
2026-02-05 21:36:01 +0100

    fix: CSS path

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -7,7 +7,7 @@ import { handleServerFunctions, RootLayout } from '@payloadcms/next/layouts'
 import React from 'react'
 
 import { importMap } from './admin/importMap.js'
-import './custom.scss'
+import '@/assets/css/payload.css';
 
 type Args = {
   children: React.ReactNode
