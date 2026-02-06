6dd75d6135ec512d8e0a26641cb2ebba3b662b51
ynamite <ynamite@bluewin.ch>
2026-02-06 11:19:51 +0100

    fix: remove header theme provider

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,14 +1,9 @@
 import type React from "react";
 
-import { HeaderThemeProvider } from "./HeaderTheme";
 import { PointerProvider } from "./PointerProvider";
 
 export const Providers: React.FC<{
   children: React.ReactNode;
 }> = ({ children }) => {
-  return (
-    <PointerProvider>
-      <HeaderThemeProvider>{children}</HeaderThemeProvider>
-    </PointerProvider>
-  );
+  return <PointerProvider>{children}</PointerProvider>;
 };
