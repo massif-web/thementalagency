f00f594398c766874a2fcc2de30860ba574ee934
ynamite <ynamite@bluewin.ch>
2026-02-13 14:32:41 +0100

    fix: remove provider

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -6,7 +6,6 @@ import { AdminBar } from "@/components/AdminBar";
 import { Footer } from "@/components/Footer/Footer";
 import { Header } from "@/components/Header/Header";
 import { Providers } from "@/providers/Providers";
-import { InitTheme } from "@/providers/Theme/ThemeProvider";
 import { mergeOpenGraph } from "@/utilities/mergeOpenGraph";
 import "@/assets/css/style.css";
 import { getServerSideURL } from "@/utilities/getURL";
