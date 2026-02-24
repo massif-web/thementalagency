9948ffb591879e41257b39f78702e3e5c796d7e2
ynamite <ynamite@bluewin.ch>
2026-02-24 19:04:51 +0100

    testin some more

--- src/components/Blocks/Hero.tsx
+++ src/components/Blocks/Hero.tsx
@@ -1,8 +1,8 @@
 import dynamic from "next/dynamic";
-import { DottedGlowBackground } from "@/components/Effects/DottedGlowBackground";
-import { Float } from "@/components/Effects/Float";
-import { MouseGradientBg } from "@/components/Effects/MouseGradientBg";
-import { NoiseOverlay } from "@/components/Effects/NoiseOverlay";
+// import { DottedGlowBackground } from "@/components/Effects/DottedGlowBackground";
+// import { Float } from "@/components/Effects/Float";
+// import { MouseGradientBg } from "@/components/Effects/MouseGradientBg";
+// import { NoiseOverlay } from "@/components/Effects/NoiseOverlay";
 import { CMSLink } from "@/components/Link";
 import { Media } from "@/components/Media/Media";
 import RichText from "@/components/RichText";
@@ -37,7 +37,7 @@ export const Hero = (hero: Page["hero"]) => {
           />
         )}
       </div> */}
-        <DottedGlowBackground
+        {/* <DottedGlowBackground
           color={"rgb(234, 196, 64)"}
           glowColor={"rgb(234, 196, 64)"}
           speedMin={0.15}
@@ -47,19 +47,19 @@ export const Hero = (hero: Page["hero"]) => {
           opacity={1}
           gap={14}
           className="z-5 isolate absolute inset-0 mask-b-from-65% mask-t-from-55% mask-radial-to-90% pointer-events-none mask-radial-at-center"
-        />
+        /> */}
         <div className="relative flex flex-1 justify-center lg:items-center w-full page-margin">
           <div className="max-lg:pt-20 w-full section-p">
             {media && typeof media === "object" && (
-              <Float className="top-0 right-0 bottom-0 lg:absolute relative lg:w-[55%] lg:h-full pointer-events-none user-select-none mix-blend-lighten">
+              <div className="top-0 right-0 bottom-0 lg:absolute relative lg:w-[55%] lg:h-full pointer-events-none user-select-none mix-blend-lighten">
                 <Media
                   className="bg-contain! size-full object-contain"
                   priority
                   resource={media}
                 />
-              </Float>
+              </div>
             )}
-            <NoiseOverlay className="z-4 isolate opacity-30 mix-blend-darken" />
+            {/* <NoiseOverlay className="z-4 isolate opacity-30 mix-blend-darken" /> */}
             <div className="z-5 relative mt-[-10%] max-w-prose">
               {richText && (
                 <RichText
@@ -77,14 +77,14 @@ export const Hero = (hero: Page["hero"]) => {
             </div>
           </div>
         </div>
-        <MouseGradientBg
+        {/* <MouseGradientBg
           id="hero"
           size="50vmin"
           opacity={1}
           blendMode="mix-blend-overlay"
           className="absolute inset-0"
           innerClassName="mask-radial-to-[68vmin] mask-radial-at-center [--color:var(--color-accent)]"
-        />
+        /> */}
       </div>
       {keywords.length > 0 && (
         <Marquee
